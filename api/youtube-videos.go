8049140a2e5480aaf42e4e62fbf73b9165f47f11
Arjun Mahishi <arjun.mahishi@gmail.com>
2020-08-17 00:01:02 +0530

    Allow all origins to access the API again

--- api/youtube-videos.go
+++ api/youtube-videos.go
@@ -39,6 +39,8 @@ var (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	resp, err := http.Get(fmt.Sprintf(baseURL, key, maxResults))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
