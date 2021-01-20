83e4652172193dd7da7a24885b9d94195abc004e
Arjun Mahishi <arjun.mahishi@gmail.com>
2021-01-21 00:09:05 +0530

    ✨Add: support to show multiple channels with common search

--- main.js
+++ main.js
@@ -4,13 +4,26 @@ const youtubeMaxResult = "50";
 const maxDisplay = 10;
 const baseURL = "https://link-in-bio.vercel.app"; // prod
 // const baseURL = "http://localhost:3000"; // dev
+
+const CHANNELS = [
+  {
+    "name": "music",
+    "youtubePlaylistID": "UU9eTgNyhtPaVf7h-YEo-R2w",
+    "youtubeChannelID": "UC9eTgNyhtPaVf7h-YEo-R2w"
+  },
+  {
+    "name": "tech",
+    "youtubePlaylistID": "UUr1ZnTuLNaB6VGeb7W0Eb1g",
+    "youtubeChannelID": "UCr1ZnTuLNaB6VGeb7W0Eb1g",
+  }
+]
 var cardTemplate;
 
-const getVideos = (resp) => {
+const getVideos = (playlistID, resp) => {
     let xhr = new XMLHttpRequest();
     xhr.open(
-        'GET', 
-        `${baseURL}/api/youtube-videos?yt_playlist_id=${youtubePlaylistID}&yt_max_results=${youtubeMaxResult}`    
+        'GET',
+        `${baseURL}/api/youtube-videos?yt_playlist_id=${playlistID}&yt_max_results=${youtubeMaxResult}`
     );
     xhr.send();
     xhr.onload = function() {
@@ -22,40 +35,50 @@ const getVideos = (resp) => {
     };
 }
 
-const getCard = (video) => {   
+const getCard = (video) => {
     let card = cardTemplate
-    for (attr in video) {       
+    for (attr in video) {   
         if(attr == 'title'){
             var miniTitle = video[attr].split('|');
             card = card.replaceAll(`{{${attr}}}`, miniTitle[0]);
         }else{
             card = card.replaceAll(`{{${attr}}}`, video[attr]);
-        }        
+        }    
     }
+    card = card.replaceAll(`{{}}`, video[attr]);
     return card
 }
 
 const handleChange = (e) => {
-    document.querySelector("#cards-holder").innerHTML = "";
-    displayCards(listOfVideo.filter((video) => 
+  CHANNELS.forEach(channel => {
+    document.querySelector(`#${channel.name}-cards-holder`).innerHTML = "";
+    displayCards(channel.name, channel.videos.filter((video) =>
         video.title.toLowerCase().includes(e.value.toLowerCase())));
+  })
 }
 
-const displayCards = (videos) => {
-    videos.slice(0,maxDisplay-1).forEach(video => {
-        document.querySelector("#cards-holder").innerHTML += getCard(video);
-    });
-    document.querySelector("#cards-holder").style.visibility = "visible"; 
-    document.querySelector("#loader").style.visibility = "hidden";
+const displayCards = (channelName, videos) => {
+  videos.slice(0,maxDisplay-1).forEach(video => {
+      document.querySelector(`#${channelName}-cards-holder`).innerHTML += getCard(video);
+  });
+  document.querySelector(`#${channelName}-cards-holder`).style.visibility = "visible";
+  document.querySelector("#loader").style.visibility = "hidden";
 }
 
-var listOfVideo;
 window.onload = function() {
-    document.querySelector("#cards-holder").style.visibility = "hidden"; 
-    document.querySelector("#loader").style.visibility = "visible";
-    cardTemplate = document.querySelector("#card-template").innerHTML;
-    getVideos(videos => {
-        listOfVideo = videos;
-        displayCards(videos);
-    });    
+  document.querySelectorAll(".card-list").forEach(list => {
+    list.style.visibility = "hidden";
+  })
+  document.querySelector("#loader").style.visibility = "visible";
+  cardTemplate = document.querySelector("#card-template").innerHTML;
+
+  CHANNELS.forEach(channel => {
+    document.querySelector("#cards-holder").innerHTML += `
+      <section id="${channel.name}-cards-holder" class="card-list" ></section>        
+    `
+    getVideos(channel.youtubePlaylistID, videos => {
+      channel["videos"] = videos;
+      displayCards(channel.name, videos);
+    });
+  })
 }
